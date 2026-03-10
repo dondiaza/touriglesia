c9b703e334131c057a8b353b05e6b4b6b31a87cf
dondiaza <49620813+dondiaza@users.noreply.github.com>
2026-03-10 10:45:01 +0100

    chore: add src/components/maps/index.ts

---
+++ src/components/maps/index.ts
@@ -0,0 +1,6 @@
+export { default as GoogleMapView } from "./GoogleMapView";
+export { default as LeafletMap } from "./LeafletMap";
+export { default as MapboxMap } from "./MapboxMap";
+export { default as MapLibreMap } from "./MapLibreMap";
+export { default as OpenLayersMap } from "./OpenLayersMap";
+export type { BaseMapProps, MapCoordinates, MapMarker } from "./types";
